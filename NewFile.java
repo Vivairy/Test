eaa97e673fd4a818ba09c64ca36c6cec65af3414
Vivairy <69755681+Vivairy@users.noreply.github.com>
2021-03-16 11:59:18 +0400

    I am adding cadence and speed
    I added 2 fields and 1 method per each.

--- NewFile.java
+++ NewFile.java
@@ -3,6 +3,16 @@ public class NewFile {
 // I am adding the comment to the test branch
 // I am adding the comment to the main branch
 // I am adding another comment to the test branch
+    int speed = 0;
+    int cadence = 0;
+    
+    void speedUp (int increment) {
+      speed += increment:
+    }
+    
+    void changeCadence (int newValue) {
+      cadence = newValue;
+    }
 
   }
 }
