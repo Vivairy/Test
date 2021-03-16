22bf939a207af11d57c2e7922621d28945f9d0a6
Vivairy <69755681+Vivairy@users.noreply.github.com>
2021-03-16 12:00:50 +0400

    I am adding gear
    I added 1 field and 1 method for gear

--- NewFile.java
+++ NewFile.java
@@ -3,6 +3,11 @@ public class NewFile {
 // I am adding the comment to the test branch
 // I am adding the comment to the main branch
 // I am adding another comment to the test branch
+    int gear = 1;
+    
+    void changeGear (int newValue) {
+      gear = newValue;
+    }
 
   }
 }
